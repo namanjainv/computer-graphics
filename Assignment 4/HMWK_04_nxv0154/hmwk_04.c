c0e63ff16c9b4175c02a28ae2d69cda55e177825
Naman Jain <namanjain@Namans-MacBook-Air.local>
2019-10-30 01:33:42 -0500

    completed assignment 4

--- Assignment 4/HMWK_04_nxv0154/hmwk_04.c
+++ Assignment 4/HMWK_04_nxv0154/hmwk_04.c
@@ -46,9 +46,29 @@ int main( int argc, char *argv[] )
   // Draw lines corresponding to the viewport region.
   Line line;
 
-  // TODO: "Draw" the four edges of the portal region as
-  //       given in the view structure.  Use a Line
-  //       structure and the dumpLine() routine.
+  line.p1X = v->m_portalXMin;
+  line.p1Y = v->m_portalYMin;
+  line.p2X = v->m_portalXMin;
+  line.p2Y = v->m_portalYMax;
+  dumpLine(&line);
+
+  line.p1X = v->m_portalXMin;
+  line.p1Y = v->m_portalYMax;
+  line.p2X = v->m_portalXMax;
+  line.p2Y = v->m_portalYMax;
+  dumpLine(&line);
+
+  line.p1X = v->m_portalXMax;
+  line.p1Y = v->m_portalYMax;
+  line.p2X = v->m_portalXMax;
+  line.p2Y = v->m_portalYMin;
+  dumpLine(&line);
+
+  line.p1X = v->m_portalXMax;
+  line.p1Y = v->m_portalYMin;
+  line.p2X = v->m_portalXMin;
+  line.p2Y = v->m_portalYMin;
+  dumpLine(&line);
 
   //--------------------------------------
   // Check each line of each triangle.
@@ -56,13 +76,31 @@ int main( int argc, char *argv[] )
 
   for ( int i=0; i<m->m_numFaces; i++ ) {
 
-    // TODO: Check each line of each face to see if
-    //       it should be drawn.  If so, draw it.
-    //       Use a Line structure to represent the
-    //       the line, the clipLine() routine to
-    //       clip it, and the dumpLine() routine to
-    //       "draw" it (if necessary).
+    int v1 = m->m_face[i].v1;
+    int v2 = m->m_face[i].v2;
+    int v3 = m->m_face[i].v3;
 
+
+    line.p1X = m->m_vertex[v1].x;
+    line.p1Y = m->m_vertex[v1].y;
+    line.p2X = m->m_vertex[v2].x;
+    line.p2Y = m->m_vertex[v2].y;
+    if(clipLine(v, &line) == 1)
+      dumpLine(&line);
+
+    line.p1X = m->m_vertex[v2].x;
+    line.p1Y = m->m_vertex[v2].y;
+    line.p2X = m->m_vertex[v3].x;
+    line.p2Y = m->m_vertex[v3].y;
+    if(clipLine(v, &line) == 1)
+      dumpLine(&line);
+
+    line.p1X = m->m_vertex[v3].x;
+    line.p1Y = m->m_vertex[v3].y;
+    line.p2X = m->m_vertex[v1].x;
+    line.p2Y = m->m_vertex[v1].y;
+    if(clipLine(v, &line) == 1)
+      dumpLine(&line);
   }
 
   //--------------------------------------
--- Assignment 4/HMWK_04_nxv0154/line.c
+++ Assignment 4/HMWK_04_nxv0154/line.c
@@ -21,13 +21,46 @@ int _regionCode( double x, double y, View *v );
 //----------------------------------------------------------
 int clipLine( View *v, Line *l )
 {
-  // TODO: Clip the line l against the portal limits
-  //       in the view using the Cohen-Sutherland
-  //       method as described in class.
-  //       If p1 or p2 of the line is updated, ensure
-  //       that you change it in the Line structure.
-  //       Return whether the line should be drawn
-  //       at all (0 for no, 1 for yes).
+  int p1_region = _regionCode(l->p1X, l->p1Y, v);
+  int p2_region = _regionCode(l->p2X, l->p2Y, v);
+  if ( (p1_region == p2_region) && (p1_region == INSIDE) )
+    return 1;
+  if( ( p1_region & p2_region) != INSIDE) {
+    printf("Line Clipped");
+    dumpLine(l);
+    return 0;
+  }
+  
+  int l_region = ( p1_region == INSIDE )? p2_region : p1_region;
+  int pt = ( p1_region == INSIDE )? 2 : 1;
+  double x = 0;
+  double y = 0;
+  
+  if(l_region & ABOVE) {
+    x = l->p1X + ( l->p2X - l->p1X ) * ( v->m_portalYMax - l->p1Y ) * ( l->p2Y - l->p1Y );
+    y = v->m_portalYMax;
+  }
+  else if(l_region & BELOW) {
+    x = l->p1X + ( l->p2X - l->p1X ) * ( v->m_portalYMin - l->p1Y ) * ( l->p2Y - l->p1Y );
+    y = v->m_portalYMin;
+  }
+  else if(l_region & RIGHT) {
+    x = v->m_portalXMax;
+    y = l->p1Y + ( l->p2Y - l->p1Y ) * ( v->m_portalXMax - l->p1X ) * ( l->p2X - l->p1X );
+  }
+  else if(l_region & LEFT) {
+    x = v->m_portalXMin;
+    y = l->p1Y + ( l->p2Y - l->p1Y ) * ( v->m_portalXMin - l->p1X ) * ( l->p2X - l->p1X );
+  }
+  if(pt == 1) {
+    l->p1X = x;
+    l->p1Y = y;
+  }
+  else {
+    l->p2X = x;
+    l->p2Y = y;
+  }
+  return clipLine(v, l);
 }
 
 //----------------------------------------------------------
@@ -40,11 +73,17 @@ void dumpLine( Line *l )
 //----------------------------------------------------------
 int _regionCode( double x, double y, View *v )
 {
-  // TODO: Compute the region code for the point x, y
-  //       by comparing against the portal limits in
-  //       the view.  (Don't forget to use EPSILON
-  //       comparisons!)
-  //       Return the computed code.
+  int CODE = INSIDE;
+
+  if( x < v->m_portalXMin ) 
+    CODE = CODE | LEFT;
+  else if( x > v->m_portalXMax )
+    CODE = CODE | RIGHT;
+  if( y < v->m_portalYMin ) 
+    CODE = CODE | BELOW;
+  else if( y > v->m_portalYMax )
+    CODE = CODE | ABOVE;
+  return CODE;
 }
 
 //----------------------------------------------------------
